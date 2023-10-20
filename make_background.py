1f7f1ce0d89d7a8104d55dcef540cca311dce450
feemster <feemster@usna.edu>
2023-10-20 11:03:49 -0400

    Status as of 20-Oct-2023

--- make_background.py
+++ make_background.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -11,13 +12,51 @@ for ii in range(0, 10):
 print('\nRunning make_background.py.')
 print('-------------------------------------------\n')
 
-# Screen dimensions
+# Specify screen dimensions.
 scr_wid = 800  # (px)
 scr_hgt = 600  # (px)
 
-# Create the screen
+# Create the screen.
 scr = pygame.display.set_mode((scr_wid, scr_hgt))
 pygame.display.set_caption('Making a customized background')
+
+
+# ------------------------------------------------------------------------
+def make_background(surface):
+    # Load the images.
+    water = pygame.image.load("assets/sprites/water.png").convert()
+    sand = pygame.image.load("assets/sprites/sand.png").convert()
+    sand_top = pygame.image.load("assets/sprites/sand_top.png").convert()
+    seagrass = pygame.image.load("assets/sprites/seagrass.png").convert()
+
+    # Makes black pixels transparent.
+    sand_top.set_colorkey((0, 0, 0))
+    seagrass.set_colorkey((0, 0, 0))
+
+    # Cover screen with water.
+    for x in range(0, surface.get_width(), water.get_width()):
+        for y in range(0, surface.get_height(), water.get_height()):
+            surface.blit(water, (x, y))
+
+    # Draw base sand at bottom of screen.
+    for x in range(0, surface.get_width(), sand.get_width()):
+        surface.blit(sand, (x, surface.get_height() - sand.get_height()))
+
+    # Draw top sand (has divots) on top of base sand.
+    for x in range(0, surface.get_width(), sand_top.get_width()):
+        surface.blit(sand_top, (x, surface.get_height() - sand.get_height() - sand_top.get_height()))
+
+    # Draw seagrass.
+    for _ in range(0, 5):
+        x = random.randint(0, surface.get_width() - seagrass.get_width())
+        surface.blit(seagrass, (x, surface.get_height() - sand.get_height() - sand_top.get_height()
+                                - seagrass.get_height() + 5))
+
+# ------------------------------------------------------------------------
+
+
+background = scr.copy()
+make_background(background)
 
 running = True
 while running:
@@ -26,6 +65,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    scr.blit(background, (0, 0))
 
     # Update the display
     pygame.display.flip()
@@ -37,5 +78,3 @@ print('End of line.')
 # Quit Pygame
 pygame.quit()
 sys.exit()
-
-
