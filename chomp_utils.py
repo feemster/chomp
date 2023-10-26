57122641024eebeeccbd76c8936faadde486e88e
feemster <feemster@usna.edu>
2023-10-25 20:30:16 -0400

    Added class for fish.
    Added the Fish class.

--- chomp_utils.py
+++ chomp_utils.py
@@ -1,13 +1,14 @@
 import pygame
 import random
+import time
 
 
 def make_background(surface):
     # Load the images.
-    water = pygame.image.load("assets/sprites/water.png").convert()
-    sand = pygame.image.load("assets/sprites/sand.png").convert()
-    sand_top = pygame.image.load("assets/sprites/sand_top.png").convert()
-    seagrass = pygame.image.load("assets/sprites/seagrass.png").convert()
+    water = pygame.image.load('assets/sprites/water.png').convert()
+    sand = pygame.image.load('assets/sprites/sand.png').convert()
+    sand_top = pygame.image.load('assets/sprites/sand_top.png').convert()
+    seagrass = pygame.image.load('assets/sprites/seagrass.png').convert()
 
     # Makes black pixels transparent.
     sand_top.set_colorkey((0, 0, 0))
@@ -31,3 +32,15 @@ def make_background(surface):
         x = random.randint(0, surface.get_width() - seagrass.get_width())
         surface.blit(seagrass, (x, surface.get_height() - sand.get_height() - sand_top.get_height()
                                 - seagrass.get_height() + 5))
+
+
+def make_splash_screen(background, scr):
+    custom_font = pygame.font.Font('assets/fonts/Black_Crayon.ttf', 128)
+    text = custom_font.render('Chomp', False, (255, 69, 0))
+    scr.blit(background, (0, 0))
+    scr.blit(text, (scr.get_width() / 2 - text.get_width() / 2, scr.get_height() / 2 - text.get_height() / 2 - 100))
+    # Update the display (show to player).
+    pygame.display.flip()
+
+    print('SPLASH SCREEN!!!!!!')
+    time.sleep(5)
